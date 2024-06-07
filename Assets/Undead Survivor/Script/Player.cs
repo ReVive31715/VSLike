3903f3afa2d14edeab3c8cbe5b0a112f04a0ba5f
ReVive31715 <128776778+ReVive31715@users.noreply.github.com>
2024-06-07 14:13:26 +0900

    Added Animation
    To Video #3

--- Assets/Undead Survivor/Script/Player.cs
+++ Assets/Undead Survivor/Script/Player.cs
@@ -9,10 +9,14 @@ public class Player : MonoBehaviour
     public float speed;
 
     Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
     private void FixedUpdate()
     {
@@ -22,5 +26,14 @@ public class Player : MonoBehaviour
     private void OnMove(InputValue value)
     {
         inputVec = value.Get<Vector2>();
+    }
+
+    private void LateUpdate()
+    {
+        animator.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0) {
+            spriteRenderer.flipX = inputVec.x < 0;
+        }
     }
 }
