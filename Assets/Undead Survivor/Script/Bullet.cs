58b002a096a48d64b490be19499d3a2ab880fe43
ReVive31715 <128776778+ReVive31715@users.noreply.github.com>
2024-09-13 13:48:50 +0900

    원거리 무기 제작
    강의 영상 8강까지.

---
+++ Assets/Undead Survivor/Script/Bullet.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float damage;
+    public int penetration;
+
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    public void Init(float damage, int penetration, Vector3 dir)
+    {
+        this.damage = damage;
+        this.penetration = penetration;
+
+        if (penetration > -1) 
+        {
+            rigid.velocity = dir * 7;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Enemy") || penetration == -1)
+        {
+            return;
+        }
+
+        penetration--;
+
+        if (penetration == -1)
+        {
+            rigid.velocity = Vector2.zero;
+            gameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Area"))
+        {
+            rigid.velocity = Vector2.zero;
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Undead Survivor/Script/Weapon.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public int id;
+    public int prefabId;
+    public float damage;
+    public int count;
+    public float speed;
+
+    float timer;
+    Player player;
+
+    void Awake()
+    {
+        player = GetComponentInParent<Player>();
+    }
+
+    void Start()
+    {
+        Init();
+    }
+    void Update()
+    {
+        switch (id)
+        {
+            case 0:
+                transform.Rotate(Vector3.back * speed * Time.deltaTime);
+
+                break;
+            default:
+                timer += Time.deltaTime;
+
+                if (timer > speed)
+                {
+                    timer = 0f;
+                    Fire();
+                }
+                break;
+
+        }
+
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            LevelUp(20, 5);
+        }
+    }
+
+    public void LevelUp(float damage, int count)
+    {
+        this.damage = damage;
+        this.count += count;
+
+        if (id == 0)
+        {
+            Place();
+        }
+    }
+    public void Init()
+    {
+        switch (id)
+        {
+            case 0:
+                speed = 150;
+                Place();
+
+                break;
+            default:
+                speed = 0.3f;
+                break;
+
+        }
+    }
+
+    void Place()
+    {
+        for (int index = 0; index < count; index++)
+        {
+            Transform bullet;
+
+            if (index < transform.childCount)
+            {
+                bullet = transform.GetChild(index);
+            }
+            else
+            {
+                bullet = GameManager.instance.pool.Get(prefabId).transform;
+                bullet.parent = transform;
+            }
+
+            bullet.localPosition = Vector3.zero;
+            bullet.localRotation = Quaternion.identity;
+
+            Vector3 rotationVector = Vector3.forward * 360 * index / count;
+            bullet.Rotate(rotationVector);
+            bullet.Translate(bullet.up * 1.5f, Space.World);
+            bullet.GetComponent<Bullet>().Init(damage, -1, Vector3.zero); //-1Àº ¹«ÇÑ.
+        }
+    }
+
+    void Fire()
+    {
+        if (!player.scanner.nearestTarget)
+        {
+            return; 
+        }
+
+        Vector3 targetPos = player.scanner.nearestTarget.position;
+        Vector3 dir =  targetPos - transform.position;
+        dir = dir.normalized;
+
+        Transform bullet = GameManager.instance.pool.Get(prefabId).transform;
+        bullet.position = transform.position;
+        bullet.rotation = Quaternion.FromToRotation(Vector3.up, dir);
+        bullet.GetComponent<Bullet>().Init(damage, count, dir);
+
+    }
+}
